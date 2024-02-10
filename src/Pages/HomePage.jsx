7b0dd5243044609542fb3e66f69ce7ee936627b8
Ragul <121421581+Ragul-9862@users.noreply.github.com>
2024-02-10 10:56:20 +0530

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Band from  '../Components/Band'
 import SpeedOmeter from '../Components/SpeedOmeter'
-import Banner from '../Components/Banner'
 import View360 from '../Components/View360'
 import Highlights from '../Components/Highlights'
 import OurBlogs from '../Components/OurBlogs'
 import Footer from '../Components/Footer'
-import New from '../Components/new'
 
 export default function HomePage() {
   return (
     <div>
         <Navbar/>
-        <Banner/>
+        <Band/>
         <SpeedOmeter/>
         <View360/>
-        {/* <New/> */}
         <Highlights/>
         <OurBlogs/>
         <Footer/>
