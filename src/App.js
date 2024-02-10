5c440e4a20c708b8d184580578601cbf6f9d7ecb
Ragul <121421581+Ragul-9862@users.noreply.github.com>
2024-02-10 11:00:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import './App.css';
 import './Responsive.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import HomePage from './Pages/HomePage';
-import Editable from './Components/Editable';
-import Blogsnew from './Components/Blogsnew';
-import Editterr from './Components/Editterr';
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        {/* <Route path='/' element={<HomePage/>}/> */}
-       <Route path='/' element={<Blogsnew/>}/>
+       <Route path='/' element={<HomePage/>}/>
       </Routes>
       </BrowserRouter>
      
